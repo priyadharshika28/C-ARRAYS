e4a9875fec2a472904b09bfe2c071c099cd677e8
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2025-07-02 17:49:25 +0530

    Create sum of array after removing duplicates.c

---
+++ sum of array after removing duplicates.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int sum=0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]==arr[j]){
+                for(int k=j;k<n;k++){
+                    arr[k]=arr[k+1];
+                }
+                n--;
+                j--;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    printf("%d",sum);
+}
