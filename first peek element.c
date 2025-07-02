85a477fe69f16c8d14578a8a0389111026a8cda7
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2025-07-02 16:53:26 +0530

    Create first peek element.c

---
+++ first peek element.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int count =0;
+    for(int i=0;i<n;i++){
+        if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+            count++;
+            if(count ==1){
+                printf("%d",arr[i]);
+                break;
+            }
+        }
+    }
+}
